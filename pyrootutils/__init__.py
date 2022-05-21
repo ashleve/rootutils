1044a9c7887daf1dc40d81b3cc2cce538ba0f848
ashleve <zalewski.ukas@gmail.com>
2022-05-21 14:07:00 +0200

    Add setup.py

---
+++ pyrootutils/__init__.py
@@ -0,0 +1,3 @@
+from .pyrootutils import get_from_root, get_root, set_root, setup_root
+
+__all__ = ["get_root", "set_root", "setup_root", "get_from_root"]
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    name="pyrootutils",
+    version="0.0.0",
+    license="MIT",
+    description="A simple python package to solve all of your problems with pythonpath, working directory, file paths and module imports.",
+    long_description=long_description,
+    author="ashleve",
+    author_email="ashlevegalaxy@gmail.com",
+    url="https://github.com/ashleve/pyrootutils",
+    packages=find_packages(),
+    python_requires=">=3.7.0",
+    install_requires=[],
+    tests_require=["pytest"],
+)
