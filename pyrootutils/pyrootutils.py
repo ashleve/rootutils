0d45e29f1fe45cf274fada0fae440cfa56add299
ashleve <zalewski.ukas@gmail.com>
2022-05-21 15:07:25 +0200

    Update methods init args

--- pyrootutils/pyrootutils.py
+++ pyrootutils/pyrootutils.py
@@ -11,17 +11,37 @@ def _pyrootutils_recursive_search(path: str, indicator: Union[str, Iterable[str]
     pass
 
 
-def get_root():
+def get_root(search_from: str, indicator: Union[str, Iterable[str]] = ".project-root") -> Path:
     pass
 
 
-def set_root():
+def set_root(
+    path: str,
+    pythonpath: bool = True,
+    cwd: bool = True,
+    project_root_env_var: bool = True,
+    dotenv: bool = True,
+):
     pass
 
 
-def setup_root():
-    pass
+def setup_root(
+    search_from: str,
+    indicator: Union[str, Iterable[str]] = ".project-root",
+    pythonpath: bool = True,
+    cwd: bool = True,
+    project_root_env_var: bool = True,
+    dotenv: bool = True,
+):
+    """Combines `get_root()` and `set_root()` into one method."""
+    path = get_root(search_from, indicator)
+    set_root(path, pythonpath, cwd, project_root_env_var, dotenv)
+    return path
 
 
-def get_from_root():
+def get_from_root(
+    search_from: str,
+    indicator: Union[str, Iterable[str]] = ".project-root",
+    filepath: str = "",
+) -> Path:
     pass
---
+++ tests/test_pyrootutils.py
@@ -0,0 +1,8 @@
+from pyrootutils import get_root, set_root, setup_root, get_from_root
+
+
+def test_pyrootutils():
+    assert get_root
+    assert set_root
+    assert setup_root
+    assert get_from_root
