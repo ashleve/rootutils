0f8af6997a78af7c88fa36177017d1c1fbf8a78a
ashleve <zalewski.ukas@gmail.com>
2022-06-08 18:18:19 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import find_packages, setup
 
-from pyrootutils import __version__
-
 with open("README.md") as f:
     long_description = f.read()
 
 
 setup(
     name="pyrootutils",
-    version=__version__,
+    version="1.0.3",
     license="MIT",
     description="Simple package for easy project root setup",
     long_description=long_description,
@@ -19,6 +17,6 @@ setup(
     packages=find_packages(),
     python_requires=">=3.7.0",
     include_package_data=True,
-    install_requires=["python-dotenv"],
+    install_requires=["python-dotenv>=0.20.0"],
     tests_require=["pytest"],
 )
